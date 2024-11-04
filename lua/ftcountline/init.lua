755581ef13810c69506e4affb385c0ea0e774fb7
kporceil <163202211+Natox10@users.noreply.github.com>
2024-11-04 16:27:04 +0100

    Updated

--- lua/ftcountline/init.lua
+++ lua/ftcountline/init.lua
@@ -76,7 +76,6 @@ function M.display_line_counts()
 		vim.api.nvim_buf_set_extmark(bufnr, namespace, func.end_line, 0, {
 			virt_text = { { string.format("Lines in function: %d", func.line_count), "Comment" } },
 			virt_text_pos = "eol",
-			hl_mode = "italic",
 		})
 	end
 end
